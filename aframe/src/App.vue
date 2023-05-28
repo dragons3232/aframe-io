8a406f60bc78d764db0d01add77519db091891cf
long.phan <long.phan@netpower.no>
2023-05-28 22:06:25 +0700

    Handle mouse enter & leave events

--- aframe/src/App.vue
+++ aframe/src/App.vue
@@ -14,7 +14,7 @@
     <a-box onclick="alert('box 1')" position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" scale="1 0.5 2" material="src: netpower.png" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-box>
     <a-entity onclick="alert('box 2')" position="-2 2 -3" rotation="0 -30 0" geometry="primitive: box; width: 0.5; height: 1; depth: 1.5" material="color: #8855aa; opacity: 0.4; src: #netpower"></a-entity>
     <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-    <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+    <a-cylinder event-set__mouseenter="opacity: 0.8" event-set__mouseleave="opacity: 1" position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
     <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
     <a-entity position="-1 4 -3" light="type: point; intensity: 0.5" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-entity>
     <a-entity id="light" light="type: ambient; color: #ccc"></a-entity>
