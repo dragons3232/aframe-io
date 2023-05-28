61a1b3237c82795fb9690b9e0ce5d3b1a8fca3a1
long.phan <long.phan@netpower.no>
2023-05-28 17:07:18 +0700

    Asset usage

--- aframe/src/App.vue
+++ aframe/src/App.vue
@@ -2,8 +2,11 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <a-scene>
+    <a-assets>
+      <img id="netpower" src="netpower.png">
+    </a-assets>
     <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" scale="1 0.5 2" material="src: netpower.png" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-box>
-    <a-entity position="-2 2 -3" rotation="0 -30 0" geometry="primitive: box; width: 0.5; height: 1; depth: 1.5" material="color: #8855aa; opacity: 0.4"></a-entity>
+    <a-entity position="-2 2 -3" rotation="0 -30 0" geometry="primitive: box; width: 0.5; height: 1; depth: 1.5" material="color: #8855aa; opacity: 0.4; src: #netpower"></a-entity>
     <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
     <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
     <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
