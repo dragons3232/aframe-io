33eb819b3d810e37e8425decd831e02c5d1fc722
long.phan <dragons3232@gmail.com>
2024-09-10 16:12:21 +0700

    Illustrate phone

--- aframe/src/App.vue
+++ aframe/src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <!-- Custom aframe component with THREE js https://jsfiddle.net/gftruj/3brL5744/ -->
   <a-scene>
     <a-assets>
-      <img id="netpower" src="netpower.png">
+      <img id="netpower" src="netpower.png" />
+      <img id="phone" src="./assets/phone.png" />
+      <img id="i1" src="./assets/1.png" />
+      <img id="i2" src="./assets/2.png" />
+      <img id="i3" src="./assets/3.png" />
+      <img id="i4" src="./assets/4.png" />
     </a-assets>
     <a-entity
       id="camera"
@@ -12,38 +17,84 @@
       rotation="0 0 0"
       animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 3000; to: 0 0 -3; loop: true; startEvents: welcomeStart"
     >
-    <a-camera
-     position="0 2 1"
-     look-controls="enabled: true"
-     cursor="fuse: false; rayOrigin: mouse;"
-     raycaster="near: 0; far: 10"
-     >
-      <a-entity cursor="fuse: true; fuseTimeout: 500"
+      <a-camera
+        position="0 0 130"
+        look-controls="enabled: true"
+        cursor="fuse: false; rayOrigin: mouse;"
+        raycaster="near: 0; far: 10"
+      >
+        <!-- <a-entity cursor="fuse: true; fuseTimeout: 500"
         position="0 0 -1"
         geometry="primitive: ring; radiusInner: 0.005; radiusOuter: 0.01"
         material="color: red">
-      </a-entity>
-    </a-camera>
+      </a-entity> -->
+      </a-camera>
     </a-entity>
-    <a-entity gltf-model="#cityModel" position="0 0 18"></a-entity>
-    <a-box onclick="alert('box 1')" position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" scale="1 0.5 2" material="src: netpower.png" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-box>
-    <a-entity onclick="alert('box 2')" position="-2 2 -3" rotation="0 -30 0" geometry="primitive: box; width: 0.5; height: 1; depth: 1.5" material="color: #8855aa; opacity: 0.4; src: #netpower"></a-entity>
-    <a-sphere cursor-listener raycaster-listen position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-    <a-cylinder onclick="app.startWelcome()" event-set__mouseenter="opacity: 0.8" event-set__mouseleave="opacity: 1" position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
-    <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-    <a-entity position="-1 4 -3" light="type: point; intensity: 0.5" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-entity>
-    <a-entity id="light" light="type: ambient; color: #ccc"></a-entity>
-    <a-sky color="#ECECEC"></a-sky>
+    <a-image
+      position="0 -5 0"
+      rotation="0 0 0"
+      height="221.7"
+      width="294.5"
+      src="#phone"
+    ></a-image>
+
+    <a-plane
+      position="-74.5 20.5 20"
+      rotation="-12 -12 -7"
+      height="18"
+      width="18"
+      src="#i1"
+      color="white"
+      transparent="true"
+    ></a-plane>
+    <a-plane
+      position="-80.5 -0.5 20"
+      rotation="-12 -12 -6"
+      height="17.5"
+      width="18"
+      src="#i2"
+      color="white"
+      transparent="true"
+    ></a-plane>
+
+    <a-plane
+      position="-86 -21.5 20"
+      rotation="-12 -5 -5.5"
+      height="17"
+      width="17"
+      src="#i3"
+      color="white"
+      transparent="true"
+    ></a-plane>
+
+    <a-plane
+      position="-92.5 -43 20"
+      rotation="-12 -5 -5"
+      height="17"
+      width="17"
+      src="#i4"
+      color="white"
+      transparent="true"
+    ></a-plane>
+    <!-- <a-entity gltf-model="#cityModel" position="0 0 18"></a-entity> -->
+    <!-- <a-box onclick="alert('box 1')" position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" scale="1 0.5 2" material="src: netpower.png" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-box> -->
+    <!-- <a-entity onclick="alert('box 2')" position="-2 2 -3" rotation="0 -30 0" geometry="primitive: box; width: 0.5; height: 1; depth: 1.5" material="color: #8855aa; opacity: 0.4; src: #netpower"></a-entity> -->
+    <!-- <a-sphere cursor-listener raycaster-listen position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere> -->
+    <!-- <a-cylinder onclick="app.startWelcome()" event-set__mouseenter="opacity: 0.8" event-set__mouseleave="opacity: 1" position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder> -->
+    <!-- <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane> -->
+    <!-- <a-entity position="-1 4 -3" light="type: point; intensity: 0.5" animation="property: position; easing: easeInOutQuad; dir: alternate; dur: 1000; to: -1 1 -3.5; loop: true"></a-entity> -->
+    <!-- <a-entity id="light" light="type: ambient; color: #ccc"></a-entity> -->
+    <a-sky color="#fffff"></a-sky>
   </a-scene>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   },
   mounted(){
     /*
